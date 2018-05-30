e397aa3994aea3387412728f3993f3dec45f1c02
M. Rummens <michel.rummens@hva.nl>
2018-05-30 17:33:34 +0200

    Cleaned code

--- Rpi code/Weigh.py
+++ Rpi code/Weigh.py
@@ -3,56 +3,55 @@ import time
 import sys
 from HX711 import HX711
 import threading
+import requests
+
+
 class Scale(threading.Thread):
     
+    # Called when Driver.py aborts.
     def cleanAndExit(self):
-        print
-        "Cleaning..."
+        print("Cleaning...")
         GPIO.cleanup()
-        print
-        "Bye!"
+        print("Bye!")
         sys.exit()
     
     def run(self):
-
-
+        print("Weigh.py = Enabled")
+        # Set datapins to 16 & 18.
         hx = HX711(16, 18)
-
-# I've found out that, for some reason, the order of the bytes is not always the same between versions of python, numpy and the hx711 itself.
-# Still need to figure out why does it change.
-# If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
-# There is some code below to debug and log the order of the bits and the bytes.
-# The first parameter is the order in which the bytes are used to build the "long" value.
-# The second paramter is the order of the bits inside each byte.
-# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
         hx.set_reading_format("LSB", "MSB")
-
-# HOW TO CALCULATE THE REFFERENCE UNIT
-# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-        hx.set_reference_unit(112)
-#hx.set_reference_unit()
-        gram = 50
+        
+        # With a reference_unit of 420, returns value in Grams.
+        hx.set_reference_unit(420)
         hx.reset()
         hx.tare()
-
+        
+        val = 0
+        oldgram = 0
+        
         while True:
+            
             try:
-        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
-        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
-        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment the three lines to see what it prints.
-        # np_arr8_string = hx.get_np_arr8_string()
-        # binary_string = hx.get_binary_string()
-        # print binary_string + " " + np_arr8_string
-
-        # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
+                
+                # Calculates average of 30 weighs.
                 val = hx.get_weight(30)
-                print(round((val/gram)), " Gram")
+                gram = (round(-val))
+                
+                if((gram < (oldgram -10)) or (gram > (oldgram +10))):
+                    print("Sending Weighdata to mendix.. Data = ", gram)
 
+##                    headers = {'Content-Type': 'application/json'}
+##                    URL = 'https://2018et08publisheds.mxapps.io/rest/Published_REST_service/Beer'
+##                    payload = "{\n\t\"Beer\": " + str(gram) + "}"
+##                    
+##                    response = requests.request("PUT", URL, data=payload, headers=headers)
+##                    print(response)
+                
+                # Sets oldgram = gram, so above IF statement is only executed once per change.
+                oldgram = gram
+                
                 hx.power_down()
                 hx.power_up()
-                time.sleep(0.5)
+                
             except (KeyboardInterrupt, SystemExit):
                 cleanAndExit()
